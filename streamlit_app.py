9f892174b6706c2d17df5b664eb9914cd4e6788d
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-10 17:55:35 +0000

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ access_token = st.secrets["FIGMA_API_KEY"] # 'your_access_token_here'
 
 # The file key from the Figma file URL
 # LINK: https://www.figma.com/design/pagv93kJSWZUwhmr6BIyVG/Figma-basics?m=auto&t=U8vfwSQqWWQO9rBO-6
-file_key = 'pagv93kJSWZUwhmr6BIyVG' # 'your_file_key_here'
 
+placeholder = 'pagv93kJSWZUwhmr6BIyVG' # 'your_file_key_here'
+file_key = placeholder # 'your_file_key_here'
 # Figma API endpoint for getting file data
 url = f'https://api.figma.com/v1/files/{file_key}'
 
@@ -19,6 +20,8 @@ response = requests.get(url, headers=headers)
 
 st.title("Figgy Putting")
 st.write("Get Figma data using the API.")
+st.text_input("Figma File ID", file_key, placeholder=file_key)
+
 # Check if the request was successful
 if response.status_code == 200:
     figma_data = response.json()
